1631c1de29746027491c9038eb612eb2d6cdefbd
jingyanlei <jingyanlei@dongqiudi.com>
2017-07-13 15:09:50 +0800

    crontab定时任务

---
+++ crontab/crontab-1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/robfig/cron"
+	"log"
+)
+
+func main() {
+	i := 0
+	c := cron.New()
+	spec := "*/5 * * * * ?"
+	c.AddFunc(spec, func() {
+		i++
+		log.Println("cron running:", i)
+	})
+	//启动计划任务
+	c.Start()
+	//关闭着计划任务, 但是不能关闭已经在执行中的任务.
+	defer c.Stop()
+	select{}
+}
